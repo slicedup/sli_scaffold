3f37fdacf23721897d0daac148e76068e267d914
Paul Webster <paul@webprogression.co.nz>
2011-03-22 22:03:34 +1300

    Test stubs

---
+++ tests/cases/models/ScaffoldModelTest.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Slicedup: a fancy tag line here
+ *
+ * @copyright	Copyright 2011, Paul Webster / Slicedup (http://slicedup.org)
+ * @license 	http://opensource.org/licenses/bsd-license.php The BSD License
+ */
+
+namespace slicedup_scaffold\tests\cases\models;
+
+class ScaffoldModelTest extends \lithium\test\Unit {}
+?>
