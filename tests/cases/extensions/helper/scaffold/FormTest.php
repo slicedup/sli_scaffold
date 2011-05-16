d67efd1d9c7672df9b443a213afa935864cb9d46
Paul Webster <paul@webprogression.co.nz>
2011-05-16 21:08:33 +1200

    Start of basic form tests

--- tests/cases/extensions/helper/scaffold/FormTest.php
+++ tests/cases/extensions/helper/scaffold/FormTest.php
@@ -8,6 +8,10 @@
 
 namespace slicedup_scaffold\tests\cases\extensions\helper\scaffold;
 
+use lithium\test;
+
+use slicedup_scaffold\extensions\helper\scaffold;
+
 use slicedup_scaffold\extensions\helper\scaffold\Form;
 
 class FormTest extends \slicedup_scaffold\tests\cases\extensions\helper\ScaffoldBaseTest {
@@ -15,8 +19,27 @@ class FormTest extends \slicedup_scaffold\tests\cases\extensions\helper\Scaffold
 	public function testBasicForm() {
 		$form = new Form(array('context' => $this->context));
 		$fieldset = $form->addFieldSet();
-		$fieldset->addField('id');
-		print($form);
+		$fields = array('id', 'name');
+		$fieldset->addField('id', array('method' => 'hidden'));
+		$fieldset->addField('name');
+		$fieldset = $form->addFieldSet(array('legend' => 'Contact'));
+		$fieldset->addField('email');
+		$fieldset->addField('phone');
+
+		$result = $form->render();
+		$this->assertTags($result, array(
+			'form' => array(
+				'action' => '/',
+				'method' => 'post',
+			),
+			'fieldset' => array(),
+			'input' => array('type' => 'hidden', 'name' => 'id'),
+			'div' => array(),
+			'label' => array('for' => 'Name'),
+			'Name',
+			'/label',
+		));
+
 	}
 }
 
