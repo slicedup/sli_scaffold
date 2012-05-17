4ec7fe454c749d5780d93639c4d751524b55baa3
Paul Webster <paul@webprogression.co.nz>
2012-05-18 10:24:15 +1200

    Add: dispatch filter to apply a placeholder translate handler used in scaffolds when g11n has not been configured

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -9,6 +9,27 @@
 use lithium\core\Libraries;
 use lithium\action\Dispatcher;
 use sli_scaffold\core\Scaffold;
+use lithium\net\http\Media;
+use lithium\util\String;
+
+/**
+ * Dispatch filter to patch the call to translate in scaffold templates that
+ * are g11n enabled, when g11n hanlders are not set
+ */
+Dispatcher::applyFilter('_callable', function($self, $params, $chain) {
+	Media::applyFilter('_handle', function($self, $params, $chain) {
+		$t = function($message, $options = array()){
+			if (!empty($options)) {
+				$message = String::insert($message, $options);
+			}
+			return $message;
+		};
+		$params['handler'] += array('outputFilters' => array());
+		$params['handler']['outputFilters'] += compact('t');
+		return $chain->next($self, $params, $chain);
+	});
+	return $chain->next($self, $params, $chain);
+});
 
 /**
  * Dispatch filter to handle scaffold requests
